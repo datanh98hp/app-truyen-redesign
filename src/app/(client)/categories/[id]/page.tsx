7bd563f3245bc100ff342050d32c6e54437e70a6
datanh98hp <dat198hp@gmail.com>
2025-05-29 16:03:03 +0700

    fix page"

--- src/app/(client)/categories/[id]/page.tsx
+++ src/app/(client)/categories/[id]/page.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 
-async function Category({
-  params,
-}: Readonly<{
-  params: { id: string };
-}>) {
+async function Category({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return <div>Category - {id}</div>;
 }
