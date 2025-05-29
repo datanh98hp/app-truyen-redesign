43c7fdbaa01845c0cfb1e1097314c0878847c9b1
datanh98hp <dat198hp@gmail.com>
2025-05-29 18:04:08 +0700

    add swiper

--- src/components/content/swipper.tsx
+++ src/components/content/swipper.tsx
@@ -8,23 +8,35 @@ export function SwipperContent() {
   return (
     <div className="max-w-screen h-fit md:w-6xl w-xl mx-2 md:mx-0">
       <Swiper
-        slidesPerView={5}
-        spaceBetween={15}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        slidesPerView={3}
+        spaceBetween={30}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
+        autoplay={{
+          delay: 2500,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+          },
+          1024: {
+            slidesPerView: 6,
+            spaceBetween: 15,
+          },
+        }}
+        modules={[Navigation, Autoplay, Pagination]}
         className="mySwiper"
       >
         {Array.from({ length: 20 }).map((_, index) => (
           <SwiperSlide key={index} className="border ">
-            <div className="h-56">Slide {index + 1}</div>
+            <div className="h-40">Slide {index + 1}</div>
           </SwiperSlide>
         ))}
       </Swiper>
